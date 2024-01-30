1fbc5dbba3e90b6258d73a34f8407b9d8aa95b9f
Zain <zhassan00512@gmail.com>
2024-01-30 21:15:46 +0500

    Your commit message here

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:month_year_picker/month_year_picker.dart';
 import 'package:e_attendance/login.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 // ...
 
 Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: "AIzaSyDrLtvq9pjBa3I5AEuoSVKJUKqy4BjBbKk",
+    options: FirebaseOptions(
+      apiKey: dotenv.env['Api_key'].toString(),
       appId: "1:455710872636:web:601e80d70e1583aa7c9f39",
       messagingSenderId: "455710872636",
       projectId: "elevate-attendance-app",
